802e4f405224827b8ea8a4e472433c49074dc9f6
saddiqs1 <ssaddique96@hotmail.com>
2026-08-16 18:14:26 +0100

    initial terraform, bootstrap to build s3 & iam permissions for gh actions, tf to build ECR, updated deploy action

---
+++ infra/main.tf
@@ -0,0 +1,36 @@
+resource "aws_ecr_repository" "app" {
+  name                 = "gymshark-saddiqs1"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep the latest 20 images"
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 20
+      }
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+output "ecr_repository_url" {
+  description = "URL used to tag and push application images"
+  value       = aws_ecr_repository.app.repository_url
+}
